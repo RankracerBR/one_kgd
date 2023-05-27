657a50dae57fedcaea7cccc0a0374afeac0e338f
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-05-27 06:31:38 -0300

    project

--- Church_Site/django_project/church_/core/admin.py
+++ Church_Site/django_project/church_/core/admin.py
@@ -1,9 +1,30 @@
+import csv
+from django.http import HttpResponse
 from django.contrib import admin
+from django.contrib.auth.models import User
 from .models import Email
-# Register your models here.
+
+def export_users_to_csv(modeladmin, request, queryset):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="usuarios.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['Email', 'Nome', 'Sobrenome'])
+
+    for user in queryset:
+        writer.writerow([user.email, user.nome, user.sobrenome])
+
+    total_users = queryset.count()
+    writer.writerow([])
+    writer.writerow(['Total de Usuários: ', total_users])
+
+    return response
+
+export_users_to_csv.short_description = 'Exportar usuários para CSV'
 
 class EmailAdmin(admin.ModelAdmin):
-    list_display = ('nome','sobrenome','email')
+    list_display = ('nome', 'sobrenome', 'email')
     list_filter = ('nome', 'sobrenome',)
+    actions = [export_users_to_csv]
 
-admin.site.register(Email,EmailAdmin)
+admin.site.register(Email, EmailAdmin)
